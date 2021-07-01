5eb7337b978abebf6475f8be909f3974da4bbe7a
viniciusoliveiras <vinitag190@gmail.com>
2021-07-01 20:22:20 -0300

    refactor: home page and login button created with Chakra-UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-use-before-define */
 import React from 'react';
 
+import { Flex, Box, Text } from '@chakra-ui/react';
 import Head from 'next/head';
 import Image from 'next/image';
 
@@ -8,7 +9,6 @@ import googleLogoImg from '../../public/images/google-logo.svg';
 import logoImg from '../../public/images/logo.svg';
 import weightliftingImg from '../../public/images/weightlifting.png';
 import { LoginButton } from '../components/LoginButton';
-import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
@@ -17,20 +17,36 @@ export default function Home() {
         <title>shape.it</title>
       </Head>
 
-      <main className={styles.container}>
-        <section className={styles.content}>
+      <Flex
+        h="100vh"
+        maxWidth="6xl"
+        margin="0 auto"
+        justify="space-between"
+        align="center"
+        px="8"
+      >
+        <Box>
           <Image src={logoImg} alt="shape.it logo" />
 
-          <h1>Salve e gerencie seus treinos com facilidade.</h1>
+          <Text
+            fontSize="6xl"
+            maxW="xl"
+            lineHeight="5rem"
+            fontWeight="semibold"
+            color="white"
+            marginTop="12"
+          >
+            Salve e gerencie seus treinos com facilidade.
+          </Text>
 
-          <LoginButton type="button">
+          <LoginButton type="button" marginTop="32">
             <Image src={googleLogoImg} alt="G" />
             Login com o Google
           </LoginButton>
-        </section>
+        </Box>
 
         <Image src={weightliftingImg} alt="Levantamento de peso" />
-      </main>
+      </Flex>
     </>
   );
 }
