a8726c069ae58d8d13a4ff4c5f3f96bb3ed831f5
viniciusoliveiras <vinitag190@gmail.com>
2021-07-07 14:40:49 -0300

    feat: add empty state workout page

--- src/pages/workouts.tsx
+++ src/pages/workouts.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable no-use-before-define */
-import { Flex } from '@chakra-ui/react';
+import { Flex, Image, Text } from '@chakra-ui/react';
 import Head from 'next/head';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Header } from '../components/Header';
 import { Sidebar } from '../components/Sidebar';
 
 export default function Workouts() {
+  const [workouts, setWorkouts] = useState(undefined);
+
   return (
     <>
       <Head>
@@ -15,8 +17,24 @@ export default function Workouts() {
 
       <Header />
 
-      <Flex mx="24">
+      <Flex mx={{ base: '6', md: '8', lg: '12', xl: '24' }}>
         <Sidebar />
+
+        {!workouts && (
+          <Flex flexDirection="column" align="center" flex="1">
+            <Image
+              src="/images/weightlifting.png"
+              alt="Weightlifting"
+              boxSize={{ base: '48', lg: '56', xl: '60' }}
+            />
+            <Text fontWeight="bold" fontSize="xl" lineHeight="7" mt="8">
+              Nenhum treino por aqui...
+            </Text>
+            <Text fontWeight="medium" fontSize="md" lineHeight="base" mt="1">
+              Crie um treino e shape it up!
+            </Text>
+          </Flex>
+        )}
       </Flex>
     </>
   );
